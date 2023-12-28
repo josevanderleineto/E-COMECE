5bd248a54c04768f3aaf014f214160377917f886
Vanderlei <jvn.neto@protonmail.com>
2023-12-28 17:20:24 -0300

    Adicionando menu

--- src/Componets/Menu/NavMenu.jsx
+++ src/Componets/Menu/NavMenu.jsx
@@ -1,15 +1,19 @@
 import { Link } from 'react-router-dom';
+import '../Menu/Nav.css';
+import home  from  '../../assets/img/home.svg';
+import user  from  '../../assets/img/user.svg';
+
 
 const NavMenu = () => {
     return(
-        <div>
+        <div >
       <nav>
-        <ul>
+        <ul className='menu'>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/"><img src={home} alt="Home" /></Link>
           </li>
           <li>
-            <Link to="/login">Login</Link>
+            <Link to="/login"><img src={user} alt="User" /></Link>
           </li>
         </ul>
       </nav>
